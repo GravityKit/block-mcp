0eec9859a287bbd4f35ddc5e8fbf89572010a2e6
Zack Katz <zack@katz.co>
2026-05-18 15:10:29 -0400

    refactor(plugin): migrate update_block_by_ref to with_post_edit_context()

--- wordpress-plugin/gk-block-api/includes/class-rest-controller.php
+++ wordpress-plugin/gk-block-api/includes/class-rest-controller.php
@@ -1838,50 +1838,34 @@ class REST_Controller {
 	 * @return \WP_REST_Response|\WP_Error
 	 */
 	public function update_block_by_ref( $request ) {
-		try {
-			$post_id    = (int) $request->get_param( 'id' );
-			$perm_check = $this->check_post_edit_permission( $post_id );
-			if ( is_wp_error( $perm_check ) ) {
-				return $perm_check;
-			}
+		return $this->with_post_edit_context(
+			$request,
+			function ( $post_id, $req ) {
+				$ref   = (string) $req->get_param( 'ref' );
+				$index = $this->block_crud->resolve_ref_to_index( $post_id, $ref );
+				if ( is_wp_error( $index ) ) {
+					return $index;
+				}
 
-			$if_match = $this->check_if_match_for_post( $post_id, $request );
-			if ( is_wp_error( $if_match ) ) {
-				return $if_match;
-			}
+				$attributes = $req->get_param( 'attributes' );
+				$inner_html = $req->get_param( 'innerHTML' );
 
-			$ref   = (string) $request->get_param( 'ref' );
-			$index = $this->block_crud->resolve_ref_to_index( $post_id, $ref );
-			if ( is_wp_error( $index ) ) {
-				return $index;
-			}
+				if ( null === $attributes && null === $inner_html ) {
+					return new \WP_Error(
+						'missing_data',
+						__( 'At least one of "attributes" or "innerHTML" must be provided.', 'gk-block-api' ),
+						array( 'status' => 400 )
+					);
+				}
 
-			$attributes = $request->get_param( 'attributes' );
-			$inner_html = $request->get_param( 'innerHTML' );
-
-			if ( null === $attributes && null === $inner_html ) {
-				return new \WP_Error(
-					'missing_data',
-					__( 'At least one of "attributes" or "innerHTML" must be provided.', 'gk-block-api' ),
-					array( 'status' => 400 )
+				return $this->block_crud->update_block(
+					$post_id,
+					$index,
+					is_array( $attributes ) ? $attributes : array(),
+					$inner_html
 				);
 			}
-
-			$result = $this->block_crud->update_block(
-				$post_id,
-				$index,
-				is_array( $attributes ) ? $attributes : array(),
-				$inner_html
-			);
-
-			if ( is_wp_error( $result ) ) {
-				return $result;
-			}
-
-			return new \WP_REST_Response( $result, 200 );
-		} catch ( \Throwable $e ) {
-			return $this->handle_error( $e );
-		}
+		);
 	}
 
 	/**
@@ -1895,35 +1879,18 @@ class REST_Controller {
 	 * @return \WP_REST_Response|\WP_Error
 	 */
 	public function delete_block_by_ref( $request ) {
-		try {
-			$post_id    = (int) $request->get_param( 'id' );
-			$perm_check = $this->check_post_edit_permission( $post_id );
-			if ( is_wp_error( $perm_check ) ) {
-				return $perm_check;
+		return $this->with_post_edit_context(
+			$request,
+			function ( $post_id, $req ) {
+				$ref   = (string) $req->get_param( 'ref' );
+				$index = $this->block_crud->resolve_ref_to_index( $post_id, $ref );
+				if ( is_wp_error( $index ) ) {
+					return $index;
+				}
+				$count = (int) $req->get_param( 'count' );
+				return $this->block_crud->delete_blocks( $post_id, $index, $count );
 			}
-
-			$if_match = $this->check_if_match_for_post( $post_id, $request );
-			if ( is_wp_error( $if_match ) ) {
-				return $if_match;
-			}
-
-			$ref   = (string) $request->get_param( 'ref' );
-			$index = $this->block_crud->resolve_ref_to_index( $post_id, $ref );
-			if ( is_wp_error( $index ) ) {
-				return $index;
-			}
-
-			$count  = (int) $request->get_param( 'count' );
-			$result = $this->block_crud->delete_blocks( $post_id, $index, $count );
-
-			if ( is_wp_error( $result ) ) {
-				return $result;
-			}
-
-			return new \WP_REST_Response( $result, 200 );
-		} catch ( \Throwable $e ) {
-			return $this->handle_error( $e );
-		}
+		);
 	}
 
 	/**
